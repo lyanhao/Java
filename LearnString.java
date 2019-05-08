1d9d92854a56e2474cc8f6112d228c69585c2f29
李 <1094361426@qq.com>
2019-05-08 21:23:58 +0800

    1

---
+++ LearnString.java
@@ -0,0 +1,11 @@
+public class LearnString {
+	public static void Add() {
+		String a = "我唉";
+		String b = "中国";
+		System.out.println("结果是" +  a + b  );
+	}
+	public static void main(String[] args) {
+		Add();
+	
+	}
+}
